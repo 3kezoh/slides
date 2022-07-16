ad52b698d4d5166bd4f723b8ffbe04c1e5276946
louispelarrey <louispelarrey@gmail.com>
2022-07-16 17:24:32 +0200

    login & register are now working, need a bit of work

--- sources/app.jsx
+++ sources/app.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
 import { auth } from "./components/firebase";
-import {
-  Route,
-  Routes,
-  Link,
-} from "react-router-dom";
+import { Route, Routes, Link } from "react-router-dom";
+import Register from "../pages/Register";
+import ForgotPassword from "../pages/ForgotPassword";
 
 const App = () => {
   const logout = () => {
@@ -15,10 +13,24 @@ const App = () => {
 
   return (
     <>
-      <Link to="/login">Login</Link>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <button className="button signout" onClick={logout}>
+            Sign out
+          </button>
+        </li>
+      </ul>
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Routes>
     </>
   );
