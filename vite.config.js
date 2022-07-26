f7c226165bf7a08b95a383f4b27d416faa685a1d
3kezoh <3kezoh@gmail.com>
2022-07-27 01:05:40 +0200

    🚸 add maskable icons

--- vite.config.js
+++ vite.config.js
@@ -39,6 +39,18 @@ export default defineConfig({
             sizes: "512x512",
             type: "image/png",
           },
+          {
+            src: "/static/maskable-icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/static/maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
         ],
       },
     }),
