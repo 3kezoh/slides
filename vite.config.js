710c730c97badbe31f9431516cfd248b22c1b7a4
3kezoh <3kezoh@gmail.com>
2022-07-26 17:53:28 +0200

    🔧 update vite build target to es2015

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { resolve } from "path";
 
 export default {
-  "root": resolve("sources"),
-  "server": {
-    "port": 8000,
-    "host": "0.0.0.0"
+  root: resolve("sources"),
+  server: {
+    port: 8000,
+    host: "0.0.0.0",
   },
-  "build": {
-    "chunkSizeWarningLimit": 1600,
+  build: {
+    chunkSizeWarningLimit: 1600,
+    target: "es2015",
   },
 };
