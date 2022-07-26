ed341e41d9277bb4a7ab06900016ed53424e8374
3kezoh <3kezoh@gmail.com>
2022-07-27 00:55:34 +0200

    🚸 add PWA

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,51 @@
-export default {
-  root: "src",
-  server: {
-    port: 8000,
-    host: "0.0.0.0",
-  },
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1600,
     target: "es2015",
     outDir: "../dist",
   },
-};
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png}"],
+      },
+      injectRegister: "auto",
+      devOptions: {
+        enabled: true,
+      },
+      includeAssets: ["apple-touch-icon.png", "favicon.ico"],
+      manifest: {
+        name: "Slides",
+        short_name: "Slides",
+        description: "Réalisez des présentations en ligne",
+        background_color: "#272727",
+        theme_color: "#121212",
+        display: "standalone",
+        orientation: "portrait",
+        start_url: "/",
+        lang: "fr",
+        icons: [
+          {
+            src: "/static/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/static/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+  root: "src",
+  server: {
+    port: 8000,
+    host: "0.0.0.0",
+  },
+});
